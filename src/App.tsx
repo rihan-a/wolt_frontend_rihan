76db5f05d7d291cd68f57e5499e62451b6028ca1
rihan_a <a.rihan@live.com>
2023-01-18 15:56:31 +0200

    import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import CalculateFees from "./components/CalculateFees/CalculateFees";
+import DeliveryFees from "./components/DeliveryFees/DeliveryFees";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main className="main-wrapper">
+            <h1>Delivery Fee Calculator</h1>
+            <CalculateFees />
+            <DeliveryFees />
+        </main>
+    );
 }
 
 export default App;
