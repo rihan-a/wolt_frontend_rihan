40551a30d34be58696cc4516dac618ac9f534b2f
rihan_a <a.rihan@live.com>
2023-01-19 16:06:13 +0200

    update DeliveryFees component

--- src/components/DeliveryFees/DeliveryFees.tsx
+++ src/components/DeliveryFees/DeliveryFees.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./DeliveryFees.css";
 
-function DeliveryFees() {
-    return <div>DeliveryFees</div>;
+interface IProps {
+    fees: number;
 }
+
+const DeliveryFees: React.FC<IProps> = (props) => {
+    return <div>Delivery Fees is {props.fees}</div>;
+};
 
 export default DeliveryFees;
