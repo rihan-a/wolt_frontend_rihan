9d0315cecfb83bf7ee45e2e655b012c87702acd5
rihan_a <a.rihan@live.com>
2023-01-21 15:37:32 +0200

    add comments and restructure for the returned jsx

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,8 +1,9 @@
-// Input component
-// Reuseable Input Tag
+// INPUT COMPONENT
+// Reuseable Input
 
 import "./Input.css";
 
+// Props types interface
 interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label: string;
     name: string;
@@ -16,6 +17,7 @@ interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<IProps> = (props) => {
+    // Function to handle input change events and pass it to the parent through props
     const onChangeHandler = (e: any) => {
         props.inputValue({ [e.target.name]: e.target.value });
     };
@@ -23,21 +25,23 @@ const Input: React.FC<IProps> = (props) => {
     return (
         <div className="input-wrapper">
             <label className="input-label">
-                {" "}
                 {props.label}:
                 {props.required && <span className="required-star">*</span>}
             </label>
-            <input
-                className="input-field"
-                name={props.name}
-                type={props.type}
-                placeholder={props.placeHolder}
-                onChange={onChangeHandler}
-                value={props.value}
-                step={props.step}
-                required={props.required}
-            />
-            <label className="input-unit"> {props.unit}</label>
+
+            <div className="input-unit-container">
+                <input
+                    className="input-field"
+                    name={props.name}
+                    type={props.type}
+                    placeholder={props.placeHolder}
+                    onChange={onChangeHandler}
+                    value={props.value}
+                    step={props.step}
+                    required={props.required}
+                />
+                <label className="input-unit"> {props.unit}</label>
+            </div>
         </div>
     );
 };
