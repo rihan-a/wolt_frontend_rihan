a906803e6175df762a3907213d69a73f3dc126d6
rihan_a <a.rihan@live.com>
2023-01-24 21:25:54 +0200

    remove form validation and error handeling

--- src/components/CalculateFees/CalculateFees.tsx
+++ src/components/CalculateFees/CalculateFees.tsx
@@ -2,7 +2,7 @@
 // a component to collect user inputs and calculate the total delivery fees
 
 import React, { useState } from "react";
-import "./CalculateFees.css";
+
 import DeliveryFeesResult from "../DeliveryFeesResult/DeliveryFeesResult";
 // import Calculate delivery fees function
 import { calculateDeliveryFees } from "./calculateDeliveryFees";
@@ -18,32 +18,28 @@ interface IState {
 
 const CalculateFees = () => {
     // store delivery fees
-    const [deliveryFees, setDeliveryFees] = useState<number | null>(0);
-    const [error, setError] = useState("");
+    const [deliveryFees, setDeliveryFees] = useState<number | undefined>();
 
     // function to calculate the tottal delivery fees based on the user input data
     const calculateFeesHandler = (inputValues: IState) => {
         // call calculate delivery fees function using the user inputs as an argument, store the returned value in a variable
-        const totalDeliveryFees: number | null =
-            calculateDeliveryFees(inputValues);
+        const totalDeliveryFees: number = calculateDeliveryFees(inputValues);
+
+        console.log(totalDeliveryFees);
 
         // check if calculateDeliveryFees function returns a truthy value
-        if (totalDeliveryFees) {
-            setError("");
-            setDeliveryFees(totalDeliveryFees);
-        } else {
-            // if calculateDeliveryFees function returns null, then throw an error
-            console.log("error");
-            setError("Input fields must be valid numbers");
-        }
+
+        setDeliveryFees(totalDeliveryFees);
+
+        // if calculateDeliveryFees function returns null, then throw an error
+        // setError("Input fields must be valid numbers");
     };
 
     return (
-        <section className="inputs-form">
+        <>
             <UserInputs calculateFees={calculateFeesHandler} />
             <DeliveryFeesResult fees={deliveryFees} />
-            <p className="error-msg">{error}</p>
-        </section>
+        </>
     );
 };
 
