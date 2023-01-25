ea04bcfbdf7e3d3f2c22799554e08c910dd98ffa
rihan_a <a.rihan@live.com>
2023-01-25 13:55:20 +0200

    add loading spinner + rewrite calculateFeesHandler into an async function

--- src/components/CalculateFees/CalculateFees.tsx
+++ src/components/CalculateFees/CalculateFees.tsx
@@ -6,7 +6,10 @@ import React, { useState } from "react";
 import DeliveryFeesResult from "../DeliveryFeesResult/DeliveryFeesResult";
 // import Calculate delivery fees function
 import { calculateDeliveryFees } from "./calculateDeliveryFees";
+import "./CalculateFees.css";
 import UserInputs from "../UserInputs/UserInputs";
+
+import { RotatingLines } from "react-loader-spinner";
 
 // types interface
 interface IState {
@@ -20,25 +23,30 @@ const CalculateFees = () => {
     // store delivery fees
     const [deliveryFees, setDeliveryFees] = useState<number | undefined>();
 
+    const [loadingSpinner, setLoadingSpinner] = useState<boolean>(false);
+
     // function to calculate the tottal delivery fees based on the user input data
-    const calculateFeesHandler = (inputValues: IState) => {
+    const calculateFeesHandler = async (inputValues: IState) => {
+        setLoadingSpinner(true);
         // call calculate delivery fees function using the user inputs as an argument, store the returned value in a variable
-        const totalDeliveryFees: number = calculateDeliveryFees(inputValues);
-
-        console.log(totalDeliveryFees);
-
-        // check if calculateDeliveryFees function returns a truthy value
-
-        setDeliveryFees(totalDeliveryFees);
-
-        // if calculateDeliveryFees function returns null, then throw an error
-        // setError("Input fields must be valid numbers");
+        const deliveryFees = await calculateDeliveryFees(inputValues);
+        setDeliveryFees(deliveryFees);
+        setLoadingSpinner(false);
     };
 
     return (
         <>
             <UserInputs calculateFees={calculateFeesHandler} />
             <DeliveryFeesResult fees={deliveryFees} />
+            <div className="loading-spinner">
+                <RotatingLines
+                    strokeColor="rgb(0, 194, 232)"
+                    strokeWidth="5"
+                    animationDuration="0.75"
+                    width="96"
+                    visible={loadingSpinner}
+                />
+            </div>
         </>
     );
 };
