bc813e1f1708c3f690f3ddcb99bae3c6216fc1ca
rihan_a <a.rihan@live.com>
2023-01-29 14:26:55 +0200

    update test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders calculate delivery fees button", () => {
     render(<App />);
     const linkElement = screen.getByText(/calculate delivery fees/i);
     expect(linkElement).toBeInTheDocument();
